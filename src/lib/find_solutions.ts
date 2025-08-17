44d1f3f2d7823476f72031eca6794eaa30cc0503
lizbh26 <ashley.liz2605@gmail.com>
2025-08-16 18:04:49 -0600

    simplified algorithm a bit, reversed initial stack to begin with 0,0

--- src/lib/find_solutions.ts
+++ src/lib/find_solutions.ts
@@ -12,35 +12,37 @@ export function findSolutions() {
     let board = new Array(...(memoizedMoves[i] ?? []));
     stack.push({ queens: [i], board });
   }
+  stack.reverse();
 
   while (stack.length > 0) {
     const state = stack.pop();
     if (!state) continue;
 
     const { queens, board } = state;
+
     if (queens.length >= QUEENS) {
       queens.sort((a, b) => a - b);
 
       if (
-        !!solutions.find(
-          (solution) => solution.toString() === queens.toString()
-        )
+        !solutions.find((solution) => solution.toString() === queens.toString())
       ) {
-        continue;
+        solutions.push(queens);
       }
-
-      solutions.push(queens);
 
       continue;
     }
 
+    // There can only be one queen per column, so it only checks one column at a time
     const column = queens.length;
+
     for (let i = 0; i < BOARD_SIZE; i++) {
+      //If square is threatened, skip
       if (board[column * BOARD_SIZE + i]) continue;
 
       const boardForThisPiece = new Array(
         ...(memoizedMoves[column * BOARD_SIZE + i] ?? [])
       );
+
       stack.push({
         queens: [...queens, column * BOARD_SIZE + i],
         board: mergeBoards(board, boardForThisPiece),
